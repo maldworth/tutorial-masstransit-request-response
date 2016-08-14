1a20d7c03661bbfdd20a810f6ace1cade2eac3c8
maldworth <m@maldworth.com>
2016-08-14 18:30:05 -0400

    Normalize Name Convention
    Fixing naming convention to match other tutorials on maldworth.com

--- StarterKit.Web.Bootstrapper/Modules/RequestClientModule.cs
+++ StarterKit.Web.Bootstrapper/Modules/RequestClientModule.cs
@@ -16,8 +16,6 @@
             builder.Register(c => new MessageRequestClient<CheckOrderStatus, OrderStatusResult>(c.Resolve<IBus>(), address, requestTimeout))
                 .As<IRequestClient<CheckOrderStatus, OrderStatusResult>>()
                 .SingleInstance();
-
-            
         }
     }
 }
